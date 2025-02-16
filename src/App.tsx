dd6814577a6d94cb9f7a02569f44228b2f440263
Matteo Pellegrino <matteo.pellegrino@buildo.io>
2025-02-16 23:18:50 +0100

    init: bootstrap + camelot wheel outer circle

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { HarmonicWheel } from './components/HarmonicWheel';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="container">
+        <HarmonicWheel />
+      </div>
+    </div>
+  );
+}
+
+export default App; 
